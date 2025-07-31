6b34cc3620deb0faa8674ee8f2149ab337bee190
joanatria <joana.tria@gcash.com>
2025-07-31 16:12:10 +0800

    Final Submission: Declaration of variables

---
+++ README.md
@@ -0,0 +1,4 @@
+## Joana S. Tria
+# Activity: Declaring Local, Static, and Instance Variables in Java
+
+![img.png](img.png)
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,47 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    int instanceBinary = 0b10_1101;
+    int instanceOctal = 072;
+    double instanceDouble = 1_234.95;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    static int staticHex = 0x1A2B;
+    static byte staticByte = 1;
+    static String staticString = "Progress, not perfection.";
+
+    public static void printLocalVariables(){
+        System.out.println("Local Variables: ");
+        int localDecimal = 100;
+        int localHex = 0xFF;
+        int localBinary = 0b1101;
+        int localOctal = 014;
+        long localLong = 10_000_000L;
+        String localString = "Every step I take gets me closer to my goal.";
+
+        System.out.println(" localDecimal = " + localDecimal);
+        System.out.println(" localHex = " + localHex);
+        System.out.println(" localBinary = " + localBinary);
+        System.out.println(" localOctal = " + localOctal);
+        System.out.println(" localLong = " + localLong);
+        System.out.println(" localString = " + localString);
+    }
+
+    public void printInstanceVariables(){
+        System.out.println("\nInstance Variables: ");
+        System.out.println(" instanceBinary = " + instanceBinary);
+        System.out.println(" instanceOctal = " + instanceOctal);
+        System.out.println(" instanceDouble = " + instanceDouble);
+    }
+
+    public static void main(String[] args) {
+        printLocalVariables();
+
+        Main main = new Main();
+        main.printInstanceVariables();
+
+        System.out.println("\nStatic Variable: ");
+        System.out.println(" staticHex = " + staticHex);
+        System.out.println(" staticByte = " + staticByte);
+        System.out.println(" staticString = " + staticString);
     }
 }
